b6e096178e5fb53b914f38d964d085a771f05180
Michelle Villavilla <mchll.vllvll@gmail.com>
2024-09-11 17:24:36 +0800

    have firebase but not authenticate, log in UI is set

---
+++ assets/Utils/Colors.js
@@ -0,0 +1,6 @@
+export default {
+    primary: '#314026',
+    secondary: '#94bf73',
+    white: '#ffffff',
+    black: '#000000'
+}
---
+++ components/FirebaseConfig.jsx
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7_FDSt0gUrgpQImVnSYj56GBG_MTs35A",
+  authDomain: "vehiqr-spott.firebaseapp.com",
+  projectId: "vehiqr-spott",
+  storageBucket: "vehiqr-spott.appspot.com",
+  messagingSenderId: "760283279034",
+  appId: "1:760283279034:web:319c6f7a4c22ca0b299c83"
+};
+
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth (app)
+export const db = getFirestore(app);
+
+
+
+
